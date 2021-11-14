42011adc84f7862b64a5ad3e4028fa08ace75fb5
thegooddoctor62 <84278464+thegooddoctor62@users.noreply.github.com>
2021-11-14 16:20:26 +0530

    Create  Print Rectangle of Asterisks.c

---
+++ Print Rectangle of Asterisks.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main ()
+// Write a program that prints the following asterisks rectangle
+// **********
+// **********
+// **********
+// **********
+// **********
+
+
+{
+
+     printf("**********\n");
+     printf("**********\n");
+     printf("**********\n");
+     printf("**********\n");
+     printf("**********\n");
+
+    return 0;
+}
