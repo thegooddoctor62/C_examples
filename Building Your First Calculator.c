0a3dc2413dd45b8a4860f0cf3af1b7f439ee07f2
thegooddoctor62 <84278464+thegooddoctor62@users.noreply.github.com>
2021-11-14 16:12:05 +0530

    Create Building Your First Calculator.c

---
+++ Building Your First Calculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main ()
+/*simple calculator using only printf command
+consider the two numbers 7 and 5 */
+{
+
+     printf("sum = %d\n",5+7);
+     printf("difference = %d\n",7-5);
+     printf("product = %d\n",7*5);
+     printf("quotient= %d\n",7/5);
+     printf("remainder = %d\n",7%5);
+
+    return 0;
+}
